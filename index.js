dc4b767de06e460e393a7d1e473e54580456e3ff
Ludson Francisco <ludsonfrancisco1010@gmail.com>
2021-12-17 13:02:05 -0300

    deploy para vercek

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use('/tabuadas', tabuadaRouter)
 
 
 //css
-app.use(express.static('public'))
+app.use(express.static(path.join((__dirname, 'public')))
 
 //view engine
 app.set('viwes', path.join(__dirname, 'views'))
